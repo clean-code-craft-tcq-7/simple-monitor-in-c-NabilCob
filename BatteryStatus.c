5bf1296f3399d68cc5e7e42b2005a60621fbaa03
NabilCob <69570628+NabilCob@users.noreply.github.com>
2022-12-13 20:28:13 +0530

    Update BatteryStatus.c
    Updated test cases

--- BatteryStatus.c
+++ BatteryStatus.c
@@ -60,6 +60,10 @@ int GetBatteryParamStatus(float value, tst_BatParamLimits limits,
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
-
+  assert(!batteryIsOk(70, 0, 0));
+  assert(batteryIsOk(45, 80, 0.8));
+  assert(batteryIsOk(0, 20, 0.8));
+ 
+ 
   return 0;
 }
